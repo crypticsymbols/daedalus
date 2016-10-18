b5e27e9e2249a2d516edd3707344140553f20239
Aaron Smith <projects@aaronsmith.co>
2016-10-18 15:49:59 -0700

    don't worry, it's still just a prototype. Adding calibration UI and backend'

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/static/index.html');
 });
 
-var maxThrottle = 2000;
-var minThrottle = 1300;
+var maxThrottle = 1900.0;
+var minThrottle = 1100.0;
 
 var mapControls = function(inputs){
   var throttle = ((maxThrottle - minThrottle) * (inputs.throttle / 100)) + minThrottle;
@@ -39,12 +39,23 @@ io.on('connection', function (socket) {
   socket.on('flightCommand', function (data) {
     mapControls(data.values);
   });
-  // 
-  // Start apm
-  // sudo ArduCopter-quad -A udp:<webapp IP>:<udpPort>
-  // 
+
+  socket.on('calibrationCommand', function (data) {
+    var value;
+    if (data.mode == 'high'){
+      value = maxThrottle / 1000;
+      console.log('Sending motor control value of '+ value)
+      motorControl.setThrottle(value, value, value, value);
+    } else if (data.mode == 'low'){
+      value = minThrottle / 1000;
+      console.log('Sending motor control value of '+ value)
+      motorControl.setThrottle(value, value, value, value);
+    }
+  });
+
   // Start video
   // uv4l --driver raspicam --auto-video_nr --encoding h264 --width 640 --height 480 --enable-server on
+
 });
 
 server.listen(HTTP_PORT);
