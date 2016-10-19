571b8a7d72ffaa068dd6b2bdb518b983a0f23520
Aaron Smith <projects@aaronsmith.co>
2016-10-18 21:47:12 -0700

    divorcing GPIO pin setup and PWM value setting

--- Navio/Examples/Servo/Servo.cpp
+++ Navio/Examples/Servo/Servo.cpp
@@ -12,30 +12,27 @@ using namespace Navio;
 
 extern "C" {
 
-    void set(float ms_1, float ms_2, float ms_3, float ms_4)
-    {
+    void setup(){
         static const uint8_t outputEnablePin = RPI_GPIO_27;
-
         Pin pin(outputEnablePin);
-
         if (pin.init()) {
             pin.setMode(Pin::GpioModeOutput);
             pin.write(0); /* drive Output Enable low */
         } else {
             fprintf(stderr, "Output Enable not set. Are you root?\n");
-    //        return 1;
         }
-
         PCA9685 pwm;
-
         pwm.initialize();
         pwm.setFrequency(FREQUENCY);
+    }
 
-            pwm.setPWMmS(MOTOR_1, ms_1);
-            pwm.setPWMmS(MOTOR_2, ms_2);
-            pwm.setPWMmS(MOTOR_3, ms_3);
-            pwm.setPWMmS(MOTOR_4, ms_4);
-
+    void set(float ms_1, float ms_2, float ms_3, float ms_4)
+    {
+        PCA9685 pwm;
+        pwm.setPWMmS(MOTOR_1, ms_1);
+        pwm.setPWMmS(MOTOR_2, ms_2);
+        pwm.setPWMmS(MOTOR_3, ms_3);
+        pwm.setPWMmS(MOTOR_4, ms_4);
     }
 
 }
