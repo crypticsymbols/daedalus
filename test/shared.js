2ae7770c1a7456e1e85dcc064d8b71bfb220669e
Aaron Smith <projects@aaronsmith.co>
2016-10-22 23:57:35 -0700

    adding another spec for throttle scaling

--- test/shared.js
+++ test/shared.js
@@ -5,7 +5,7 @@ describe('scaling values', function(){
 
   it('scales values, e.g. % to PWM range', function(){
     expect(subject.scalePercentageToValue(10, 1000, 2000)).to.equal(1100)
-    expect(subject.scalePercentageToValue(130, 1000, 2000)).to.equal(1100)
+    expect(subject.scalePercentageToValue(130, 1000, 2000)).to.equal(2300)
     expect(subject.scalePercentageToValue(37, 174, 2837)).to.equal(1159.31)
   })
 
