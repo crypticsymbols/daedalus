e99af52c0d5b420e472ab896fb60a95efe76d426
Aaron Smith <projects@aaronsmith.co>
2016-11-19 17:36:53 -0800

    mode specs

--- test/modes.js
+++ test/modes.js
@@ -4,14 +4,25 @@ var ModeHandler = require('../lib/modes/modes')
 
 describe('modes', function() {
 
-  var mh = new ModeHandler(
-    ['calibrate', 'manual'],
-    {throttle: {min: 1000, mid: 1500, max: 2000}},
-    function() {},
-    {getControlState: function() {}}
-  )
+  // var mh = new ModeHandler(
+    // ['calibrate', 'manual'],
+    // {throttle: {min: 1000, mid: 1500, max: 2000}},
+    // function() {},
+    // {getControlState: function() {}}
+  // )
+
+  var makeHandler = (modeList) => {
+    modeList = modeList || ['calibrate', 'manual']
+    return new ModeHandler(
+      modeList,
+      {throttle: {min: 1000, mid: 1500, max: 2000}},
+      function() {},
+      {getControlState: function() {}}
+    )
+  }
 
   it('fails safe when set to nonexistent mode name', function() {
+    let mh = makeHandler()
     let originalMode = mh.getMode()
     // 1) it lets us know it failed
     expect(mh.setMode({name: 'Fake mode'})).to.eql(false)
@@ -20,16 +31,22 @@ describe('modes', function() {
   })
 
   it('returns true on successful mode switch', function() {
+    let mh = makeHandler()
     expect(mh.getMode().constructor.name).to.eql('Calibrate')
     expect(mh.setMode({name: 'manual', opts: {}})).to.eql(true)
     expect(mh.getMode().constructor.name).to.eql('Manual')
   })
 
-  it('reverts on mode switch if any errors')
-  it('loads available modes at runtime')
-  it('has a default mode')
+  it('uses first mode arg as the default', function() {
+    let mh = makeHandler(['manual', 'calibrate'])
+    expect(mh.getMode().constructor.name).to.equal('Manual')
+    mh = makeHandler(['calibrate', 'manual'])
+    expect(mh.getMode().constructor.name).to.equal('Calibrate')
+  })
+
+  it('updates and persists options')
   it('has a heartbeat function')
   it('remembers the last config when switching back to a previous mode')
   it('initializes with default config from default mode')
-
+  it('reverts on mode switch if any errors')
 })
