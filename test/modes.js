ae7373ca89b0659760418d3e1f1d9a8983f77483
Aaron Smith <projects@aaronsmith.co>
2016-11-19 16:52:24 -0800

    moving available modes into config-land'

--- test/modes.js
+++ test/modes.js
@@ -4,30 +4,32 @@ var ModeHandler = require('../lib/modes/modes')
 
 describe('modes', function() {
 
+  var mh = new ModeHandler(
+    ['calibrate', 'manual'],
+    {throttle: {min: 1000, mid: 1500, max: 2000}},
+    function() {},
+    {getControlState: function() {}}
+  )
+
   it('fails safe when set to nonexistent mode name', function() {
-    let mh = new ModeHandler(null, null, {
-      getControlState: function() {}
-    })
     let originalMode = mh.getMode()
-    //
     // 1) it lets us know it failed
     expect(mh.setMode({name: 'Fake mode'})).to.eql(false)
-    //
     // 2) It did not change anything
     expect(mh.getMode()).to.eql(originalMode)
   })
 
   it('returns true on successful mode switch', function() {
-    let mh = new ModeHandler({
-      throttle: {min: 1000, mid: 1500, max: 2000}
-    }, function() {}, {
-      getControlState: function() {}
-    })
     expect(mh.getMode().constructor.name).to.eql('Calibrate')
     expect(mh.setMode({name: 'manual', opts: {}})).to.eql(true)
     expect(mh.getMode().constructor.name).to.eql('Manual')
   })
 
-  it('reverts on mode switch in any errors')
+  it('reverts on mode switch if any errors')
+  it('loads available modes at runtime')
+  it('has a default mode')
+  it('has a heartbeat function')
+  it('remembers the last config when switching back to a previous mode')
+  it('initializes with default config from default mode')
 
 })
