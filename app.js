9a54673b2c1eb7192b2191b1c0f6b649980d0f87
Aaron Smith <projects@aaronsmith.co>
2016-11-20 10:47:55 -0800

    separate vehicle config file

--- app.js
+++ app.js
@@ -6,43 +6,6 @@ var io = require('socket.io')(server)
 const HTTP_PORT = 1337
 
 var vehicle = require('./vehicleConfig')
-// vehicle setup
-var math = require('./lib/util/math')
-var util = require('./lib/util/shared')
-var opts = {
-  motorMap: {
-    // pwmChannel: {x, y, rotation}
-    3: {y: 1, x: 1, rotation: 'cw'},
-    4: {y: -1, x: -1, rotation: 'cw'},
-    5: {y: -1, x: 1, rotation: 'ccw'},
-    6: {y: 1, x: -1, rotation: 'ccw'}
-  },
-  throttle: {
-    min: 1100,
-    mid: 1500,
-    max: 1900
-  },
-  axisScale: {
-    xR: 1,
-    yR: 1,
-    zR: 1
-  },
-  inputFilters: {
-    throttle: [
-      (value) => {
-        return math.ellipticalScale(value)
-      },
-      (value, opts) => {
-        let min = opts.throttle.min
-        let max = opts.throttle.max
-        return util.scalePercentageToValue(value, min, max)
-      }
-    ]
-  }
-}
-
-var Copter = require('./lib/platforms/copter')
-var vehicle = new Copter(opts)
 
 vehicle.on('error', function(msg) {
   console.log('VEHICLE ERROR: \n', msg)
@@ -65,17 +28,6 @@ io.on('connection', function(socket) {
     vehicle.updateConfig(data)
   })
 })
-
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/static/index.html')
